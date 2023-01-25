ea4c5a3352311afe32a999083939a6033bde86b1
Patrick <papybp@gmail.com>
2023-01-25 00:04:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/flixgo'));
-app.get('/*', function(req,res){res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));});
+app.get('/*', function(req,res){res.sendFile(path.join(__dirname+'/dist/flixgo/index.html'));});
 app.listen(process.env.PORT || 8080);
